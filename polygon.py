f6c9ad25d8baec67b22952107362b37dae092c75
Owen Fei <owenfei@Outlook.com>
2019-02-16 18:21:22 -0800

    inputs with random colors

--- polygon.py
+++ polygon.py
@@ -1,25 +1,30 @@
 from math import *
 from pygame import *
+from random import *
 init()
-w = display.set_mode([600, 600])
-def polygon(length, sides):
+w = display.set_mode([1920, 1080])
+def polygon(x, y, length, sides):
     #Exterior angles float
     angle = float(360.0 / (sides * 1.0)) 
     points = []
     for i in range(0, sides):
-        x = cos(radians(angle * i)) * length + 300
-        y = sin(radians(angle * i)) * length + 300
-        points.append((int(y), int(x)))
-    w.fill((255, 255, 255))
-    draw.polygon(w, (0, 0, 0), points)
+        x = cos(radians(angle * i)) * length + x
+        y = sin(radians(angle * i)) * length + y
+        points.append((int(x), int(y)))
+    draw.polygon(w, (randint(0, 255), randint(0, 255), randint(0, 255)), points)
     display.flip()
-d = True
-while d:
-    for e in event.get():
-        if e.type == MOUSEBUTTONDOWN:
-            down = True
-        if e.type == MOUSEBUTTONUP and down:
-            length = int(input("Length of side "))
-            sides = int(input("Number of sides "))
-polygon(length, sides)
-time.wait(5000)
+polygons = []
+w.fill((255, 255, 255))
+for i in range(1, 20):
+    length = int(input("Length of side "))
+    sides = int(input("Number of sides "))
+    d = True
+    while d:
+        for e in event.get():
+            if e.type == MOUSEBUTTONDOWN:
+                down = True
+            if e.type == MOUSEBUTTONUP and down:
+                x, y = mouse.get_pos()
+                d = False
+    polygon(x, y, length, sides)
+
