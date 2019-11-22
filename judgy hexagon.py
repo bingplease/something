99fcc54f7d094de380ab07d73421f9e597403687
bingplease <47503414+bingplease@users.noreply.github.com>
2019-11-21 18:01:29 -0800

    Judgy Hexagon v0.1.0
    Added options to change the traits.
    Rotated the hexagon sideways to it would be smaller.

--- judgy hexagon.py
+++ judgy hexagon.py
@@ -11,18 +11,10 @@ Label each of the lines with a trait (should be the traits below).
 For each of the traits, rank them from 1-8 and draw the point on the line.
 The outer hexagons are higher.
 Now, connect all 6 points, and color in the polygon made by them.
-
-iq = IQ
-eq = EQ
-att = Attractiveness
-inn = Innocence
-ath = Athleticness
-art = Artisticness
-
 '''
 
-from tkinter import (Tk, Frame, Label, Scale, Canvas, Menu, N, S, E, W, NE,
-                     messagebox as msg, HORIZONTAL, Entry)
+from tkinter import (Tk, Frame, Label, Scale, Canvas, Menu, N, S, E, W,
+                     SE, SW, NE, NW, messagebox as msg, HORIZONTAL, Entry, END)
 
 class App(Tk):
     def __init__(self, *args, **kwargs):
@@ -30,6 +22,7 @@ class App(Tk):
         the ui stuff
         '''
         super().__init__(*args, **kwargs)
+        
 
         self.winfo_toplevel().title('Judgy Hexagon')
 
@@ -42,47 +35,70 @@ class App(Tk):
 
 
         #the main canvas to show the hexagon
-        self.canvas = Canvas(self, height=800, width=560)
+        self.canvas = Canvas(self, height=566, width=800)
         self.canvas.grid(row=1, column=1)
 
         
-        #the labels to make clear which side is which
-        Label(self, text='IQ', font=('Calibri', 20)).grid(row=1,
-                                                          column=1, sticky=N) 
-        Label(self, text='\n' * 6 + 'EQ',
-              font=('Calibri', 20)).grid(row=1, column=2, sticky=N)
-        Label(self, text='Attractiveness' + '\n' * 6,
-              font=('Calibri', 20)).grid(row=1, column=2, sticky=S)
-        Label(self, text='Innocence',
-              font=('Calibri', 20)).grid(row=1, column=1, sticky=S)
-        Label(self, text='Athleticism' + '\n' * 6,
-              font=('Calibri', 20)).grid(row=1, column=0, sticky=S)
-        Label(self, text='\n' * 6 + 'Artisticness',
-              font=('Calibri', 20)).grid(row=1, column=0, sticky=N)
-
         #frame to put all the sliders in
         self.sf = Frame(self)
         self.sf.grid(row=1, column=3)
 
-        self.iqscale = Scale(self.sf, from_=0, to=7, orient=HORIZONTAL)
-        self.iqscale.grid(column=1, row=0)
-        self.eqscale = Scale(self.sf, from_=0, to=7, orient=HORIZONTAL)
-        self.eqscale.grid(column=1, row=1)
-        self.attscale = Scale(self.sf, from_=0, to=7, orient=HORIZONTAL)
-        self.attscale.grid(column=1, row=2)
-        self.innscale = Scale(self.sf, from_=0, to=7, orient=HORIZONTAL)
-        self.innscale.grid(column=1, row=3)
-        self.athscale = Scale(self.sf, from_=0, to=7, orient=HORIZONTAL)
-        self.athscale.grid(column=1, row=4)
-        self.artscale = Scale(self.sf, from_=0, to=7, orient=HORIZONTAL)
-        self.artscale.grid(column=1, row=5)
+        #entries for the trait names (with default values)
+        self.sl1 = Entry(self.sf)
+        self.sl1.grid(row=0, column=0)
+        self.sl1.insert(END, 'IQ')
+        self.sl2 = Entry(self.sf, text='EQ')
+        self.sl2.grid(row=1, column=0)
+        self.sl2.insert(END, 'EQ')
+        self.sl3 = Entry(self.sf)
+        self.sl3.grid(row=2, column=0)
+        self.sl3.insert(END, 'Attractiveness')
+        self.sl4 = Entry(self.sf)
+        self.sl4.grid(row=3, column=0)
+        self.sl4.insert(END, 'Personality')
+        self.sl5 = Entry(self.sf)
+        self.sl5.grid(row=4, column=0)
+        self.sl5.insert(END, 'Athleticism')
+        self.sl6 = Entry(self.sf)
+        self.sl6.grid(row=5, column=0)
+        self.sl6.insert(END, 'Artisticness')
 
-        Label(self.sf, text='IQ').grid(row=0, column=0)
-        Label(self.sf, text='EQ').grid(row=1, column=0)
-        Label(self.sf, text='Attractiveness').grid(row=2, column=0)
-        Label(self.sf, text='Innocence').grid(row=3, column=0)
-        Label(self.sf, text='Athleticism').grid(row=4, column=0)
-        Label(self.sf, text='Artisticness').grid(row=5, column=0)
+
+        #sliders
+        self.t1scale = Scale(self.sf, from_=0, to=9, orient=HORIZONTAL)
+        self.t1scale.grid(column=1, row=0)
+        self.t2scale = Scale(self.sf, from_=0, to=9, orient=HORIZONTAL)
+        self.t2scale.grid(column=1, row=1)
+        self.t3scale = Scale(self.sf, from_=0, to=9, orient=HORIZONTAL)
+        self.t3scale.grid(column=1, row=2)
+        self.t4scale = Scale(self.sf, from_=0, to=9, orient=HORIZONTAL)
+        self.t4scale.grid(column=1, row=3)
+        self.t5scale = Scale(self.sf, from_=0, to=9, orient=HORIZONTAL)
+        self.t5scale.grid(column=1, row=4)
+        self.t6scale = Scale(self.sf, from_=0, to=9, orient=HORIZONTAL)
+        self.t6scale.grid(column=1, row=5)
+
+        #the labels to make clear which line is which trait
+        self.l1 = Label(self, text=self.sl1.get(),
+              font=('Calibri', 20))
+        self.l1.grid(row=1, column=2, sticky=E) 
+        self.l2 = Label(self, text=self.sl2.get(),
+              font=('Calibri', 20))
+        self.l2.grid(row=1, column=1, sticky=SE)
+        self.l3 = Label(self, text=self.sl3.get(),
+              font=('Calibri', 20))
+        self.l3.grid(row=1, column=1, sticky=SW)
+        self.l4 = Label(self, text=self.sl4.get(),
+              font=('Calibri', 20))
+        self.l4.grid(row=1, column=0, sticky=W)
+        self.l5 = Label(self, text=self.sl5.get(),
+              font=('Calibri', 20))
+        self.l5.grid(row=1, column=1, sticky=NW)
+        self.l6 = Label(self, text=self.sl6.get(),
+              font=('Calibri', 20))
+        self.l6.grid(row=1, column=1, sticky=NE) #:)
+
+
 
         #put the person's name in here
         self.nameentry = Entry(self.sf)
@@ -91,45 +107,48 @@ class App(Tk):
         Label(self.sf, text='\n\nName of person\n\n').grid(row=7, column=0)
         
         
-        self.namelabel = Label(self, text='Name', font=('Calibri', 20),
-                               fg='red')
-        self.namelabel.grid(row=1, column=1, sticky=NE)
+        self.namelabel = Label(self, text='Name', font=('Calibri', 20), fg='red')
+        self.namelabel.grid(row=0, column=1)
 
-        #6 lists of points for each of the 6 lines
-        self.listup = [(280, y)
-                       for y in range(400, -1, -50)]
-        self.listupright = [(280+35*((400-y)/25), y)
-                            for y in range(400, 199, -25)]
-        self.listdownright = [(280+35*((y-400)/25), y)
-                              for y in range(400, 601, 25)]
-        self.listdown = [(280, y)
-                         for y in range(400, 801, 50)]
-        self.listdownleft = [(280-35*((y-400)/25), y)
-                              for y in range(400, 601, 25)]
-        self.listupleft = [(280-35*((400-y)/25), y)
-                            for y in range(400, 199, -25)]
+        
+        #6 lists of points
+        self.listright = [(400+40*i, 200*2**0.5)
+                          for i in range(10)]
+        self.listrightdown = [(400+20*i, 200*2**0.5+i*20*2**0.5)
+                              for i in range(10)]
+        self.listleftdown = [(400-20*i, 200*2**0.5+i*20*2**0.5)
+                              for i in range(10)]
+        self.listleft = [(400-40*i, 200*2**0.5)
+                         for i in range(10)]
+        self.listleftup = [(400-20*i, 200*2**0.5-i*20*2**0.5)
+                           for i in range(10)]
+        self.listrightup = [(400+20*i, 200*2**0.5-i*20*2**0.5)
+                            for i in range(10)]
+        
 
 
-        #Draw the 8 hexagons
-        for i in range(8):
-            self.canvas.create_polygon([self.listup[i][0],
-                                       self.listup[i][1],
-                                       self.listupright[i][0],
-                                       self.listupright[i][1],
-                                       self.listdownright[i][0],
-                                       self.listdownright[i][1],
-                                       self.listdown[i][0],
-                                       self.listdown[i][1],
-                                       self.listdownleft[i][0],
-                                       self.listdownleft[i][1],
-                                       self.listupleft[i][0],
-                                       self.listupleft[i][1],],
-                                       fill='', width=1, outline='black')
-
+        
+        #Draw the 10 hexagons
+        for i in range(10):
+            self.canvas.create_polygon([self.listright[i][0],
+                                        self.listright[i][1],
+                                        self.listrightdown[i][0],
+                                        self.listrightdown[i][1],
+                                        self.listleftdown[i][0],
+                                        self.listleftdown[i][1],
+                                        self.listleft[i][0],
+                                        self.listleft[i][1],
+                                        self.listleftup[i][0],
+                                        self.listleftup[i][1],
+                                        self.listrightup[i][0],
+                                        self.listrightup[i][1],],
+                                        fill='', width=1, outline='black')
+            
     def help(self):
         msg.showinfo(title='Help', message='''
 How to use this app:
 Move around the sliders and enter a name.
+Type your 6 character traits in the 6 entries.
 Then, take a snip of the part without the sliders.
 
 Tutorial on judgy hexagons in real life:
@@ -155,12 +174,12 @@ Another app to check out: Owen Zhang''')
         '''
 
         #Get which point each level of trait is
-        iq = self.listup[t1]
-        eq = self.listupright[t2]
-        att = self.listdownright[t3]
-        pr = self.listdown[t4]
-        ath = self.listdownleft[t5]
-        ef = self.listupleft[t6]
+        t1 = self.listright[t1]
+        t2 = self.listrightdown[t2]
+        t3 = self.listleftdown[t3]
+        t4 = self.listleft[t4]
+        t5 = self.listleftup[t5]
+        t6 = self.listrightup[t6]
 
         #REmove the last shape if possible
         try:
@@ -169,18 +188,18 @@ Another app to check out: Owen Zhang''')
             pass
 
         #Draw the new polygon
-        self.last_polygon = self.canvas.create_polygon([iq[0],
-                                                        iq[1],
-                                                        eq[0],
-                                                        eq[1],
-                                                        att[0],
-                                                        att[1],
-                                                        pr[0],
-                                                        pr[1],
-                                                        ath[0],
-                                                        ath[1],
-                                                        ef[0],
-                                                        ef[1],],
+        self.last_polygon = self.canvas.create_polygon([t1[0],
+                                                        t1[1],
+                                                        t2[0],
+                                                        t2[1],
+                                                        t3[0],
+                                                        t3[1],
+                                                        t4[0],
+                                                        t4[1],
+                                                        t5[0],
+                                                        t5[1],
+                                                        t6[0],
+                                                        t6[1],],
                                                         fill='blue',
                                                         width=1,
                                                         outline='blue')
@@ -196,13 +215,26 @@ Another app to check out: Owen Zhang''')
             self.namelabel.config(text=self.nameentry.get())
 
             #Call judge() on the values of the sliders
-            self.judge(self.iqscale.get(),
-                       self.eqscale.get(),
-                       self.attscale.get(),
-                       self.innscale.get(),
-                       self.athscale.get(),
-                       self.artscale.get())
+            self.judge(self.t1scale.get(),
+                       self.t2scale.get(),
+                       self.t3scale.get(),
+                       self.t4scale.get(),
+                       self.t5scale.get(),
+                       self.t6scale.get())
+
+            #Updates the labels with trait names.
+            self.l1.config(text=self.sl1.get())
+            self.l2.config(text=self.sl2.get())
+            self.l3.config(text=self.sl3.get())
+            self.l4.config(text=self.sl4.get())
+            self.l5.config(text=self.sl5.get())
+            self.l6.config(text=self.sl6.get())
+            
+
+            
             self.update()
+
+            
         
 
 
